dca8328c7b8772a966dbf6f1469c9f2296fe9f62
Alexandru Dochioiu <alex@dochioiu.com>
2026-02-13 10:36:59 +0200

    [CHORE] Add macOS test support and improve flutter tests
    Add make test target that builds a macOS dylib and runs flutter test.
    Remove broken infinite-loop example tests and add proper encode-decode
    round-trip tests covering both single-part and multi-part URs.

--- interfaces/ur_registry_flutter/test/ur_registry_flutter_test.dart
+++ interfaces/ur_registry_flutter/test/ur_registry_flutter_test.dart
@@ -7,10 +7,8 @@ import 'package:ur_registry_flutter/native_object.dart';
 import 'package:ur_registry_flutter/registries/crypto_account.dart';
 import 'package:ur_registry_flutter/registries/crypto_hd_key.dart';
 import 'package:ur_registry_flutter/registries/crypto_psbt.dart';
-import 'package:ur_registry_flutter/registries/ethereum/eth_sign_request.dart';
 import 'package:ur_registry_flutter/registries/ethereum/eth_signature.dart';
 import 'package:ur_registry_flutter/registries/extend/crypto_multi_accounts.dart';
-import 'package:ur_registry_flutter/registries/solana/sol_sign_request.dart';
 import 'package:ur_registry_flutter/registries/solana/sol_signature.dart';
 import 'package:ur_registry_flutter/ur_decoder.dart';
 import 'package:ur_registry_flutter/ur_encoder.dart';
@@ -55,22 +53,6 @@ void main() {
     }
   });
 
-  test("generate solana transaction", () async {
-    List<int> signData = List.from(hex.decoder.convert("01020304"));
-    String path = "M/44'/501'/0'/0/1";
-    String xfp = "01020304";
-    List<int> pubkey = List.from(hex.decoder.convert("010203040506"));
-    String origin = "BitKeep";
-    int signType = SolSignRequest.transaction; // 1: Transaction, 2: Message
-    SolSignRequest solSignRequest =
-        SolSignRequest.factory(signData, path, xfp, pubkey, origin, signType);
-    UREncoder urEncoder = solSignRequest.toUREncoder();
-    String requestId = solSignRequest.uuid; //get request id;
-    while (true) {
-      // renderQR(urEncoder.nextPart());
-    }
-  });
-
   test("collect sol signature", () async {
     String ur =
         "UR:SOL-SIGNATURE/OEADTPDAGDNDCAWMGTFRKIGRPMNDUTDNBTKGFSSBJNAOHDFZTYWTOSRFTAHPRDCTRKBEGYLOGDGHJKBAFHFLAMFWLOHGHTPSSEAOZORSIMNYBBTNNBIYNLCKENBTFMEEAMSABNAEOXASJKWSWFKEKIIECKHPECCKSSPTNDZELNWFECYLDRCYHKWS";
@@ -96,16 +78,6 @@ void main() {
     nativeObject as CryptoAccount;
     List<CryptoHDKey> keys = nativeObject.getKeys();
     String masterFingerprint = nativeObject.getMasterFingerprint();
-  });
-
-  test("construct psbt", () async {
-    String psbtHex =
-        '70736274ff01009a020000000258e87a21b56daf0c23be8e7070456c336f7cbaa5c8757924f545887bb2abdd750000000000ffffffff838d0427d0ec650a68aa46bb0b098aea4422c071b2ca78352a077959d07cea1d0100000000ffffffff0270aaf00800000000160014d85c2b71d0060b09c9886aeb815e50991dda124d00e1f5050000000016001400aea9a2e5f0f876a588df5546e8742d1d87008f000000000000000000';
-    CryptoPSBT cryptoPSBT = CryptoPSBT.factory(hex.decode(psbtHex));
-    UREncoder urEncoder = cryptoPSBT.toUREncoder();
-    while (true) {
-      // renderQR(urEncoder.nextPart());
-    }
   });
 
   test("receive psbt", () async {
@@ -134,23 +106,6 @@ void main() {
     key.getChainCode(); //chain code
     key.getBip32Xpub(); //get extended public key
     key.getSourceFingerprint(); //get master fingerprint
-  });
-
-  test("construct eth tx", () async {
-    List<int> signData = List.from(hex.decoder.convert("01020304"));
-    String path = "M/44'/60'/0'/0/0";
-    String xfp = "01020304";
-    int chainId = 0;
-    String address = "address";
-    String origin = "BitKeep";
-    int signType = EthSignRequest.typedTransaction;
-    EthSignRequest ethSignRequest = EthSignRequest.factory(
-        signData, signType, chainId, path, xfp, address, origin);
-    UREncoder urEncoder = ethSignRequest.toUREncoder();
-    String requestId = ethSignRequest.uuid; //get request id;
-    while (true) {
-      // renderQR(urEncoder.nextPart());
-    }
   });
 
   test("get eth signature", () async {
